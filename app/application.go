2176c34e580e5e0a6ad61ab0aba9b201fb5d4110
VietAnh <vanh00@VietAnhs-MacBook-Air.local>
2025-07-09 20:47:24 +0700

    update

--- app/application.go
+++ app/application.go
@@ -1,8 +1,13 @@
 package app
 
-import "MedSearch/app/crawler"
+import (
+	"MedSearch/app/crawler"
+	"MedSearch/app/database"
+)
 
 func Start() {
+	database.Connect("mongodb://localhost:27017", "medsearch")
+
 	crawler := crawler.Crawler{}
 	crawler.Start()
 }
