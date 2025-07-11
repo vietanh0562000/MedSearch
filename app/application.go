245ea402d8538d40eccb6b746576b163dac26261
VietAnh <vanh00@VietAnhs-MacBook-Air.local>
2025-07-11 09:48:02 +0700

    fix create indexes

--- app/application.go
+++ app/application.go
@@ -4,10 +4,12 @@ import (
 	"MedSearch/app/config"
 	"MedSearch/app/crawler"
 	"MedSearch/app/database"
+	"MedSearch/app/database/repository"
 )
 
 func Start() {
 	database.Connect("mongodb://localhost:27017", "medsearch")
+	repository.CreateDrugTextIndex()
 
 	config := config.Config{
 		BaseURL:  "https://nhathuoclongchau.com.vn",
